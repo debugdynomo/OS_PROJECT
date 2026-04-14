67dd6ddb00290c2050c021ffccceef724c9bd5ac
vishnu teja <vishnutejavasam.com>
2026-04-14 22:04:16 +0530

    refactor: bug fix in file structure

--- G22_Project2_Multithreading/src/logger.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-
-int logger_init(const char *log_file_path);
-void log_operation(const char *operation, const char *filepath, const char *status, const char *detail);
-void logger_close(void);
-
-#endif
